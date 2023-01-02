4d18387ddff65d82e6c8feba028ae46ff119c387
gwsc <111397774+gw-sc@users.noreply.github.com>
2023-01-02 00:05:11 +0000

    upd

---
+++ assets/js/TableCsv.js
@@ -0,0 +1,11 @@
+export default class {
+    /**
+     * @Param {HTMLTableElement} root The table element that will display the csv data
+     * 
+     */
+
+    constructor(root) {
+        this.root = root;
+        console.log("success!");
+    }
+}
